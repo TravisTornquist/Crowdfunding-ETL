da60cb1d5763d4f47af3292ef6aff9d6f51473c7
Travis Tornquist <travis.tornquist@gmail.com>
2022-12-09 21:05:35 -0500

    adding challenge files

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,4 +1,75 @@
--- create tables on the schema building website
+-- create tables in pg admin
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(100)   NOT NULL,
+    "description" text   NOT NULL,
+    "goal" numeric(10,2)   NOT NULL,
+    "pledged" numeric(10,2)   NOT NULL,
+    "outcome" varchar(50)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(10)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "categories" (
+    "category_id" varchar(10)   NOT NULL,
+    "category" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_categories" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "categories" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategories" ("subcategory_id");
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
+
+
+-- create tables in quickdb
 campaign
 -
 cd_id int PK
@@ -33,51 +104,10 @@ first_name varchar(50)
 last_name varchar(50)
 email varchar(100)
 
--- create tables in pgadmin
--- create campaign, contacts, subcategory, and category tables
-CREATE TABLE campaign (
-	cd_id INT NOT NULL,
-	contact_id INT NOT NULL,
-	company_name VARCHAR(100) NOT NULL,
-	description TEXT NOT NULL,
-	goal NUMERIC(10,2) NOT NULL,
-	pledged NUMERIC(10,2) NOT NULL,
-	outcome VARCHAR(50) NOT NULL,
-	backers_count INT NOT NULL,
-	country VARCHAR(10) NOT NULL,
-	currency VARCHAR(10) NOT NULL,
-	launched_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	category_id VARCHAR(10) NOT NULL,
-	subcategory_id VARCHAR(10) NOT NULL,
-	PRIMARY KEY (cd_id),
-	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
-	FOREIGN KEY (category_id) REFERENCES category (category_id),
-	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
-);
-
-
-
-CREATE TABLE category (
-	category_id VARCHAR(10) NOT NULL,
-	category VARCHAR(50) NOT NULL,
-	PRIMARY KEY (category_id)
-);
-
-
-CREATE TABLE subcategory (
-	subcategory_id VARCHAR(10) NOT NULL,
-	subcategory VARCHAR(50) NOT NULL,
-	PRIMARY KEY (subcategory_id)
-);
-
-CREATE TABLE contacts (
-	contact_id INT NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	PRIMARY KEY (contact_id)
-);
-
-
-SELECT* FROM contacts
+backers
+-
+backer_id varchar(10) pk
+cf_id int FK >- campaign.cf_id
+first_name varchar(50)
+last_name varchar(50)
+email varchar(100)
